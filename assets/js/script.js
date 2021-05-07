3c9afc5292f24e6d564c9eca8711b5841fb5ca9d
Brad Samuel <bradsamuel97@gmail.com>
2021-05-06 22:25:07 -0400

    saving additions to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,71 @@
 // Assignment Code
 //special characters array
-var specialChars = ["", "#"];
+function generatePassword() {
+  let specialChars = ["@", "#","$","&","!"];
+  let numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upperCase = ["A","B","C","D","E","F","G","H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let passLength;
 
-var numbArray = ["1", "2"];
+  do{
+    passLength = prompt("How many characters would you like in your password? Choose between 8 and 128.","");
+  } while (passLength < 8 || passLength > 128);
 
-var lowerCase = ["a", "b"];
-// look up split method
-var upperCase = ["A","B"];
+  let characterArray = [];
 
+  let specialConfirm;
+  let numberConfirm;
+  let lowerConfirm;
+  let upperConfirm;
 
-//Function to prompt the user fo password options
-  //create a variable to store the length of the password
-  var length = parseInt (
-    prompt('How many characters would you like your password to be?')
-  )
-  //create conditional statement to check if password length is actually a number
-  if (Number.isNaN(lenght)){
-    alert('password must be a number');
-    return null;
+  do{
+    alert("Please decide what character types you would like, you need to choose at least one.");
+    specialConfirm = confirm("Would you like special characters?");
+    numberConfirm = confirm("Would you like number characters?");
+    lowerConfirm = confirm("Would you like lowercase characters?");
+    upperConfirm = confirm("Would you like uppercase characters?");
+  } while (specialConfirm === false && numberConfirm === false && lowerConfirm === false && upperConfirm === false);
+
+  if (specialConfirm === true) {
+    for (let char of specialChars) {
+      characterArray.push(char);
+    }
   }
 
-  //conditional statement to check if password is at least 8 chars 
-  if(length <  8) {
-    alert("password lenght must be at least 8 characters")
-    return null;
-  };
+  if (numberConfirm === true) {
+    for (let char of numbArray) {
+      characterArray.push(char);
+    }
+  }
 
+  if (lowerConfirm === true) {
+    for (let char of lowerCase) {
+      characterArray.push(char);
+    }
+  }
 
-  //Conditional statement to check if password length is at least 128 characters
-  if(length < 128) {
-    alert("password lenght must be at least 128 characters")
-    return null;
-  };
+  if (upperConfirm === true) {
+    for (let char of upperCase) {
+      characterArray.push(char);
+    }
+  }
 
-  //create confirmation prompts for the type od characters the user wants to use in their password.
+  let finalPass = "";
 
-  //need to create a conditional statement to check if user included at least 1 of the char types. Return user back to start of application
+  for (let i = 0; i < passLength; i++) {
+    let selector = Math.floor(Math.random() * characterArray.length);
+    let character = characterArray[selector];
+    finalPass += character;
+  }
+  return finalPass;
+};
 
-
-var generateBtn = document.querySelector("#generate");
+  let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
